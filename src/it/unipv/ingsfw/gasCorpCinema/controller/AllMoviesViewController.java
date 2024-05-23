0af6b98491cb8a6f232fc865773a9f73a951a736
Giovicampa <151393201+Giovicampa@users.noreply.github.com>
2024-05-23 10:05:18 +0200

    Costruzione:Aggiunto AllMoviesViewController

---
+++ src/it/unipv/ingsfw/gasCorpCinema/controller/AllMoviesViewController.java
@@ -0,0 +1,25 @@
+package it.unipv.ingsfw.gasCorpCinema.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import it.unipv.ingsfw.gasCorpCinema.model.Admin;
+import it.unipv.ingsfw.gasCorpCinema.model.movie.Movie;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+
+public class AllMoviesViewController implements Initializable {
+	
+	@FXML
+	private ListView<Movie> myListView;
+	
+	private Admin admin = new Admin();
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		myListView.getItems().addAll(admin.getAllMovies());
+	}
+
+}
