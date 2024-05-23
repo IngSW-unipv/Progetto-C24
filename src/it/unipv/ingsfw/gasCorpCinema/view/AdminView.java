4cadb8f67a62fef3a29c5b67bbdc6ab9d5e42c8e
Giovicampa <151393201+Giovicampa@users.noreply.github.com>
2024-05-23 09:41:14 +0200

    Costruzione:Implementazione della AdminView

---
+++ src/it/unipv/ingsfw/gasCorpCinema/view/AdminView.java
@@ -0,0 +1,25 @@
+package it.unipv.ingsfw.gasCorpCinema.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class AdminView extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Parent root = FXMLLoader.load(getClass().getResource("AdminView.fxml"));
+		Scene scene = new Scene(root);
+		Image icon = new Image("Logo G.A.S. CORP CINEMA .png");
+		
+		stage.setScene(scene);
+		stage.setTitle("G.A.S. CORP CINEMA");
+		stage.getIcons().add(icon);
+		stage.show();
+	}
+}
