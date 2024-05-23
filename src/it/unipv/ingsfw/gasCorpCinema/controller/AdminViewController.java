113bd0e6685cf46c0bd143d8b23a8bf3612eed4f
Giovicampa <151393201+Giovicampa@users.noreply.github.com>
2024-05-23 10:37:06 +0200

    Costruzione:Prima implementazione AdminViewController

---
+++ src/it/unipv/ingsfw/gasCorpCinema/controller/AdminViewController.java
@@ -0,0 +1,34 @@
+package it.unipv.ingsfw.gasCorpCinema.controller;
+
+import it.unipv.ingsfw.gasCorpCinema.view.AddMovieView;
+import it.unipv.ingsfw.gasCorpCinema.view.AllMoviesView;
+import it.unipv.ingsfw.gasCorpCinema.view.RemoveMovieView;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+
+public class AdminViewController {	
+	
+	private Stage stage;
+	
+	public void swtichToAddFilmView(ActionEvent event) throws Exception {
+		
+		stage = new Stage();
+		AddMovieView v = new AddMovieView();
+		v.start(stage);
+	}
+	
+	public void swtichToRemoveFilmView(ActionEvent event) throws Exception {
+		
+		stage = new Stage();
+		RemoveMovieView v = new RemoveMovieView();
+		v.start(stage);
+	}
+	
+	public void switchToAllFilmView(ActionEvent event) throws Exception {
+		
+		stage = new Stage();
+		AllMoviesView v = new AllMoviesView();
+		v.start(stage);
+	}
+}
