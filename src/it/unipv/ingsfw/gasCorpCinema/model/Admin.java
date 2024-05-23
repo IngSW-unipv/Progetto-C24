eb881cbb3ea865bd01a934fc9855746937540ea7
Giovicampa <151393201+Giovicampa@users.noreply.github.com>
2024-05-23 14:42:13 +0200

    Costruzione:Implementazione metodi in Admin e AddProjectionView

--- src/it/unipv/ingsfw/gasCorpCinema/model/Admin.java
+++ src/it/unipv/ingsfw/gasCorpCinema/model/Admin.java
@@ -4,13 +4,18 @@ import java.util.List;
 import it.unipv.ingsfw.gasCorpCinema.model.movie.IMovieDAO;
 import it.unipv.ingsfw.gasCorpCinema.model.movie.Movie;
 import it.unipv.ingsfw.gasCorpCinema.model.movie.MovieDAO;
+import it.unipv.ingsfw.gasCorpCinema.model.projection.IProjectionDAO;
+import it.unipv.ingsfw.gasCorpCinema.model.projection.Projection;
+import it.unipv.ingsfw.gasCorpCinema.model.projection.ProjectionDAO;
 
 public class Admin {
 	
 	private IMovieDAO movieDAO;
+	private IProjectionDAO projectionDAO;
 	
 	public Admin() {
 		movieDAO = new MovieDAO();
+		projectionDAO = new ProjectionDAO();
 	}
 	
 	public Movie getMovieByTitle(String title) {
@@ -34,5 +39,22 @@ public class Admin {
 		for(Movie movie : movies) {
 			System.out.println(movie);
 		}
+	}
+	
+	public Projection getProjectionByHall(int idHall) {
+		return projectionDAO.getProjectionByHall(idHall);
+	}
+	
+	public List<Projection> getAllProjections(){
+		List<Projection> projections = projectionDAO.getAllProjections();
+		return projections;
+	}
+	
+	public void createProjection(Projection projection) {
+		projectionDAO.createProjection(projection);
+	}
+	
+	public void deleteProjection(Projection projection) {
+		projectionDAO.deleteProjection(projection);
 	}
 }
