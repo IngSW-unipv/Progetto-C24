4196b5ab1d3f79a5968b43d3798c8ed7bf8a4fe5
Giovicampa <151393201+Giovicampa@users.noreply.github.com>
2024-05-23 09:26:43 +0200

    Costruzione:Creazione classe MovieDAO e prima implementazione dei metodi

---
+++ src/it/unipv/ingsfw/gasCorpCinema/model/movie/MovieDAO.java
@@ -0,0 +1,124 @@
+package it.unipv.ingsfw.gasCorpCinema.model.movie;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import it.unipv.ingsfw.gasCorpCinema.DBConnection;
+
+public class MovieDAO implements IMovieDAO{
+	
+	private String schema;
+	private Connection conn;
+	
+	public MovieDAO() {
+		this.schema = "cinema";
+	}
+	
+	@Override
+	public Movie getMovieByTitle(String title) {
+
+		Movie movie = null;
+		
+		conn=DBConnection.startConnection(conn,schema);
+		PreparedStatement st1;
+		ResultSet rs1;
+		
+	    try{
+	    	String query = "SELECT * FROM movies WHERE title = ?";
+	    	
+	    	st1 = conn.prepareStatement(query);
+	    	st1.setString(1, title);
+	    	
+	    	rs1=st1.executeQuery(query);
+	    	
+	        while(rs1.next()) {
+	        	
+	          movie = new Movie(rs1.getString(2),rs1.getString(3),rs1.getInt(4),rs1.getInt(5));
+	        }
+	    } catch (SQLException e) {
+	      e.printStackTrace();
+	    }
+	    
+	    DBConnection.closeConnection(conn);
+	    return movie;
+	}
+
+	@Override
+	public List<Movie> getAllMovies() {
+		
+		List<Movie> movies = new ArrayList<>();
+		
+		conn=DBConnection.startConnection(conn,schema);
+		Statement st1;
+		ResultSet rs1;
+		
+	    try {
+	    	st1 = conn.createStatement();
+	    	String query = "SELECT * FROM movies";
+	        rs1 = st1.executeQuery(query);
+	        
+	      	while (rs1.next()) {
+	      		
+	        Movie movie = new Movie(rs1.getString(2),rs1.getString(3),rs1.getInt(4),rs1.getInt(5));
+	        movies.add(movie);
+	      	}
+	      	
+	    } catch (SQLException e) {
+	      e.printStackTrace();
+	    }
+	    
+	    DBConnection.closeConnection(conn);
+	    return movies;
+	}
+
+	@Override
+	public void insertMovie(Movie movie) {
+		
+		conn=DBConnection.startConnection(conn,schema);
+		PreparedStatement st1;
+		
+		
+	    try {
+	    
+	    	String query = "INSERT INTO movies (title, genre, duration, year) VALUES (?, ?, ?, ?)";
+	    	st1 = conn.prepareStatement(query);
+	    	
+	    	st1.setString(1, movie.getTitle());
+	    	st1.setString(2, movie.getGenre());
+	    	st1.setInt(3, movie.getDuration());
+	    	st1.setInt(4, movie.getYear());
+	    	
+	    	st1.executeUpdate();
+	    	
+	    } catch (SQLException e) {
+	      e.printStackTrace();
+	    }
+		
+	    DBConnection.closeConnection(conn);
+	}
+
+	@Override
+	public void deleteMovie(Movie movie) {
+		
+		conn=DBConnection.startConnection(conn,schema);
+		PreparedStatement st1;
+		
+	    try {
+	    	
+	    	String query = "DELETE FROM movies WHERE title = ?";
+	    	st1 = conn.prepareStatement(query);
+	    	
+	    	st1.setString(1, movie.getTitle());
+	    	st1.executeUpdate();
+	    	
+	    } catch (SQLException e) {
+	      e.printStackTrace();
+	    }
+	    
+	    DBConnection.closeConnection(conn);
+	  }
+}
