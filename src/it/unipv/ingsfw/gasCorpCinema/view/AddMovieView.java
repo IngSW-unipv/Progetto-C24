6fb24be5a3bf88bbe8a49f0dd0e962533f69cc5c
Giovicampa <151393201+Giovicampa@users.noreply.github.com>
2024-05-25 10:09:55 +0200

    Costruzione:Aggiunta file css per migliorare la grafica

--- src/it/unipv/ingsfw/gasCorpCinema/view/AddMovieView.java
+++ src/it/unipv/ingsfw/gasCorpCinema/view/AddMovieView.java
@@ -1,9 +1,12 @@
 package it.unipv.ingsfw.gasCorpCinema.view;
 
+import java.nio.file.Paths;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class AddMovieView extends Application {
@@ -13,8 +16,15 @@ public class AddMovieView extends Application {
 		// TODO Auto-generated method stub
 		Parent root = FXMLLoader.load(getClass().getResource("AddMovieView.fxml"));
 		Scene scene = new Scene(root);
+		String imagePath = Paths.get("Resources/Logo G.A.S. CORP CINEMA .png").toUri().toString();
+        Image icon = new Image(imagePath);
+        
+		String css = this.getClass().getResource("AdminView.css").toExternalForm();
+		scene.getStylesheets().add(css);
 		
 		stage.setScene(scene);
+		stage.setTitle("G.A.S. CORP CINEMA");
+		stage.getIcons().add(icon);
 		stage.show();
 	}
 
--- src/it/unipv/ingsfw/gasCorpCinema/view/AdminView.java
+++ src/it/unipv/ingsfw/gasCorpCinema/view/AdminView.java
@@ -1,5 +1,7 @@
 package it.unipv.ingsfw.gasCorpCinema.view;
 
+import java.nio.file.Paths;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,11 +17,16 @@ public class AdminView extends Application {
 		
 		Parent root = FXMLLoader.load(getClass().getResource("AdminView.fxml"));
 		Scene scene = new Scene(root);
-		//Image icon = new Image("/../Progetto-C24/Resources/Logo G.A.S. CORP CINEMA .png");
+		//Image icon = new Image("Logo G.A.S. CORP CINEMA .png");
+		String imagePath = Paths.get("Resources/Logo G.A.S. CORP CINEMA .png").toUri().toString();
+        Image icon = new Image(imagePath);
+		
+		String css = this.getClass().getResource("AdminView.css").toExternalForm();
+		scene.getStylesheets().add(css);
 		
 		stage.setScene(scene);
 		stage.setTitle("G.A.S. CORP CINEMA");
-		//stage.getIcons().add(icon);
+		stage.getIcons().add(icon);
 		stage.show();
 	}
 }
