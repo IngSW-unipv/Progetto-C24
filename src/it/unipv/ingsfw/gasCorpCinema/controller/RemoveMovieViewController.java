29b857204dcde0c8bcd3bd1aefdfeb848068506c
Giovicampa <151393201+Giovicampa@users.noreply.github.com>
2024-05-25 10:27:38 +0200

    Costruzione:Continuo css per grafica

--- src/it/unipv/ingsfw/gasCorpCinema/controller/RemoveMovieViewController.java
+++ src/it/unipv/ingsfw/gasCorpCinema/controller/RemoveMovieViewController.java
@@ -4,28 +4,49 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import it.unipv.ingsfw.gasCorpCinema.model.Admin;
 import it.unipv.ingsfw.gasCorpCinema.model.movie.Movie;
+import it.unipv.ingsfw.gasCorpCinema.model.projection.Projection;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 
 public class RemoveMovieViewController implements Initializable {
 	
 	@FXML
-	private ChoiceBox<Movie> myChoiceBox;
+	private ListView<Movie> myListView;
+	@FXML
+	private Label myLabel;
+	@FXML
+	private Button myButton;
 	
-	private Admin admin = new Admin();
+	Movie selectedMovie;
+	Admin admin = new Admin();
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		myChoiceBox.getItems().addAll(admin.getAllMovies());
-		myChoiceBox.setOnAction(this::DeleteMovie);
+		myListView.getItems().addAll(admin.getAllMovies());
+		
+		myListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Movie>(){
+
+			@Override
+			public void changed(ObservableValue<? extends Movie> arg0, Movie arg1, Movie arg2) {
+				// TODO Auto-generated method stub
+				
+				selectedMovie = myListView.getSelectionModel().getSelectedItem();
+				myLabel.setText(String.valueOf(selectedMovie));
+			}
+		});
 	}
 	
-	public void DeleteMovie(ActionEvent event) {
-		Movie myMovie = myChoiceBox.getValue();
-		admin.deleteMovie(myMovie);
+	public void removeMovie() {
+		admin.deleteMovie(selectedMovie);
 	}
 	
 }
