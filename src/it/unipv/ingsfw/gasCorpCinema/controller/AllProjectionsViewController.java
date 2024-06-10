abb28f783d901bc078fde8beb16ffcc6c2494aa3
Giovicampa <151393201+Giovicampa@users.noreply.github.com>
2024-06-10 23:18:14 +0200

    Costruzione:Prima versione del nuovo AllProjectionsView

--- src/it/unipv/ingsfw/gasCorpCinema/controller/AllProjectionsViewController.java
+++ src/it/unipv/ingsfw/gasCorpCinema/controller/AllProjectionsViewController.java
@@ -7,6 +7,7 @@ import it.unipv.ingsfw.gasCorpCinema.model.Admin;
 import it.unipv.ingsfw.gasCorpCinema.model.cinemaHall.CinemaHall;
 import it.unipv.ingsfw.gasCorpCinema.model.movie.Movie;
 import it.unipv.ingsfw.gasCorpCinema.model.projection.Projection;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
@@ -31,5 +32,27 @@ public class AllProjectionsViewController implements Initializable {
 		myListView.getItems().addAll(admin.getAllProjections());
 		
 		myChoiceBoxHall.getItems().addAll(admin.getAllHalls());
+		myChoiceBoxMovie.getItems().addAll(admin.getAllMovies());
+		myChoiceBoxDate.getItems().addAll(admin.getAllDatesWithAProjection());
+		
+		myChoiceBoxHall.setOnAction(this::getIdHall);	
+		myChoiceBoxMovie.setOnAction(this::getMovie);	
+		myChoiceBoxDate.setOnAction(this::getDate);	
+
+	}
+	
+	public void getIdHall(ActionEvent event) {
+		myListView.getItems().clear();
+		myListView.getItems().addAll(admin.getprojectionsByHall(myChoiceBoxHall.getValue().getIdHall()));
+	}
+
+	private void getMovie(ActionEvent actionevent1) {
+		myListView.getItems().clear();
+		myListView.getItems().addAll(admin.getprojectionsByMovie(myChoiceBoxMovie.getValue().getTitle()));
+	}
+	
+	private void getDate(ActionEvent actionevent1) {
+		myListView.getItems().clear();
+		myListView.getItems().addAll(admin.getprojectionsByDate(myChoiceBoxDate.getValue()));
 	}
 }
