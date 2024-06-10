12917401fd9f435abebafe513f59d71b85570fb3
Giovicampa <151393201+Giovicampa@users.noreply.github.com>
2024-06-10 23:33:42 +0200

    Costruzione:Aggiunta bottone logout

--- src/it/unipv/ingsfw/gasCorpCinema/controller/AdminViewController.java
+++ src/it/unipv/ingsfw/gasCorpCinema/controller/AdminViewController.java
@@ -4,11 +4,15 @@ import it.unipv.ingsfw.gasCorpCinema.view.AddMovieView;
 import it.unipv.ingsfw.gasCorpCinema.view.AddProjectionView;
 import it.unipv.ingsfw.gasCorpCinema.view.AllMoviesView;
 import it.unipv.ingsfw.gasCorpCinema.view.AllProjectionsView;
+import it.unipv.ingsfw.gasCorpCinema.view.FirstPageView;
 import it.unipv.ingsfw.gasCorpCinema.view.RemoveMovieView;
 import it.unipv.ingsfw.gasCorpCinema.view.RemoveProjectionView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 
@@ -26,6 +30,20 @@ public class AdminViewController {
         emailLabel.setText(email);
     }
 	
+    public void logout() throws Exception {
+    	
+    	Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Logout");
+		alert.setHeaderText("Stai per effettuare il logout");
+		alert.setContentText("Dopo aver fatto il logout verrai riportato alla homepage.");
+		
+		if(alert.showAndWait().get()== ButtonType.OK) {
+			stage = new Stage();
+			FirstPageView v = new FirstPageView();
+			v.start(stage);		
+		}
+    }
+    
 	public void swtichToAddFilmView(ActionEvent event) throws Exception {
 		
 		stage = new Stage();
