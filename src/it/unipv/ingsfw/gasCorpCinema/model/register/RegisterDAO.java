45dd834a87371c1da134dd5cc52b09a7642b732e
Giovicampa <151393201+Giovicampa@users.noreply.github.com>
2024-06-16 22:44:05 +0200

    Costruzione:Sistemata la data e ora nel register

--- src/it/unipv/ingsfw/gasCorpCinema/model/register/RegisterDAO.java
+++ src/it/unipv/ingsfw/gasCorpCinema/model/register/RegisterDAO.java
@@ -4,7 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.text.DateFormat;  
-import java.text.SimpleDateFormat; 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import it.unipv.ingsfw.gasCorpCinema.DBConnection;
 import it.unipv.ingsfw.gasCorpCinema.model.projection.Projection;
@@ -28,12 +29,18 @@ public class RegisterDAO implements IRegisterDAO{
 	    
 	    	String query = "INSERT INTO register (purchaseDateAndTime, movieTitle, projectionDate, projectionTime,ticketNumber, price, total) VALUES (?, ?, ?, ?, ?, ?, ?)";
 	    	st1 = conn.prepareStatement(query);
-	    	//DateFormat date = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss"); 
-	    	//da sistemare formato data
-	    	String dateString = String.valueOf(System.currentTimeMillis());
+	    	
+	    	 // Ottieni l'istante corrente
+	        Date currentDate = new Date();
+
+	        // Crea un formato per la data e l'ora desiderato
+	        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+	        // Formatta la data corrente secondo il formato specificato
+	        String dateTimeString = dateFormat.format(currentDate);
 	    	
 	    	//da controllare formati date e time
-	    	st1.setString(1, dateString );	
+	    	st1.setString(1, dateTimeString );	
 	    	st1.setString(2, projection.getMovieTitle());
 	    	st1.setString(3, String.valueOf(projection.getDate()));
 	    	st1.setString(4, String.valueOf(projection.getTime()));
