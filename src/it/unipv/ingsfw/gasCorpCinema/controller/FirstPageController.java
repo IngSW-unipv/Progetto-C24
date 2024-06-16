4319d578dbfc011fed83cc5a6b95b9df7462723d
adamm <adamm@192.168.200.90>
2024-06-17 00:25:39 +0200

    Costruzione: sistemata view di FirstPage.fxml

--- src/it/unipv/ingsfw/gasCorpCinema/controller/FirstPageController.java
+++ src/it/unipv/ingsfw/gasCorpCinema/controller/FirstPageController.java
@@ -15,10 +15,14 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class FirstPageController {
 	
+	@FXML
+	private ImageView imageCinema;
 	@FXML
     private TextField tf_username;
     @FXML
--- src/it/unipv/ingsfw/gasCorpCinema/view/FirstPageView.java
+++ src/it/unipv/ingsfw/gasCorpCinema/view/FirstPageView.java
@@ -18,8 +18,6 @@ public class FirstPageView extends Application {
 		Scene scene = new Scene(root);
 		String imagePath = Paths.get("Resources/Logo G.A.S. CORP CINEMA.png").toUri().toString();
 	    Image icon = new Image(imagePath);
-	    String imagePath1 = Paths.get("Resources/Logo G.A.S. CORP.png").toUri().toString();
-	    Image icon1 = new Image(imagePath1);
 			
 		stage.setScene(scene);
 		stage.setTitle("HomePage G.A.S. CORP CINEMA");
